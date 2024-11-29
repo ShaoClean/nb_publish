0d483e2648562fe1bafacfe081c0778446bcdd7e
clean <sczclean@gmail.com>
2024-11-29 15:28:07 +0800

    chore: remove test hostname and port

--- packages/nb_server/src/connect/connect.service.ts
+++ packages/nb_server/src/connect/connect.service.ts
@@ -7,8 +7,8 @@ export class ConnectService {
     constructor(private httpService: HttpService) {}
 
     async sendConnectRequest(params: SendConnectRequestDTO) {
-        const hostname = params.hostname || process.env.NB_SERVER_RECIVE_HOST;
-        const port = params.port || +process.env.NB_SERVER_TEST_PORT;
+        const hostname = params.hostname;
+        const port = params.port;
         const url = `http://${hostname}:${port}/connect/receive`;
         console.log('request url:', url);
         const res = await this.httpService.post(url, params).toPromise();
